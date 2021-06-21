505281b66a5b538af039dc9d6f51d52de927958c
vcaselli <vcasellidev@gmail.com>
2021-06-21 18:00:22 -0300

    Project created

---
+++ src/main/java/br/com/victorcaselli/projetozup/ProjetozupApplication.java
@@ -0,0 +1,13 @@
+package br.com.victorcaselli.projetozup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetozupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetozupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victorcaselli/projetozup/ProjetozupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorcaselli.projetozup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetozupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
